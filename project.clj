8bdc5c6b849202ca92850a72e33eeb235b4e8d6f
George Pittarelli <g@gjp.cc>
2016-03-12 20:54:15 -0500

    fix up lwjgl dependencies
    build should work now
    
    Signed-off-by: George Pittarelli <g@gjp.cc>

--- project.clj
+++ project.clj
@@ -4,23 +4,31 @@
   :license {:name "GNU General Purpose License version 3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"
             :distribution :repo}
+
   :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [seesaw "1.4.5"]
                  [aleph "0.4.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.lwjgl.lwjgl/lwjgl "2.9.3"]
+                 [org.lwjgl.lwjgl/lwjgl "2.9.3"
+                  :exclusions [[org.lwjgl.lwjgl/lwjgl-platform]]]
                  [org.lwjgl.lwjgl/lwjgl_util "2.9.3"]
                  [org.lwjgl.lwjgl/lwjgl_util_applet "2.9.3"]
                  [stylefruits/gniazdo "0.4.0"]
                  [slick2d "2013.01.05"]
                  [smee/binary "0.5.1"]
-                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.3"
-                  :classifier "natives-linux"
-                  :native-prefix ""]]
-  :git-dependencies [["https://github.com/jolby/colors.git"]]
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.3" :classifier "natives-linux" :native-prefix ""]
+                 ]
+
   :main ^:skip-aot clagar.core
   :target-path "target/%s"
+  :native-path "target/native"
+
+  :source-paths ["src"]
+
+  :jvm-opts ["-Djava.library.path=target/native"]
+  :git-dependencies [["https://github.com/jolby/colors.git"]]
   :profiles {:uberjar {:aot :all}})
